927747b2c3cc1fb0b82f81110ff36794ccb47fef
Seeker114 <159248655+Seeker114@users.noreply.github.com>
2024-04-08 13:38:40 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -2,6 +2,13 @@ let express = require('express');
 let app = express();
 
 
+let path = __dirname + "/public"
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", function(req, res) {
+  res.json({ message: "Hello json" });
+});
+
 
 
 
