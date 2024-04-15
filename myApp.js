2a636a83c16805b14309970c7d71efa5405e52e8
Seeker114 <159248655+Seeker114@users.noreply.github.com>
2024-04-15 13:06:45 +0000

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-
+var bodyParser = require('body-parser')
 
 
 app.use(function(req, res, next) {
@@ -11,6 +11,8 @@ app.use(function(req, res, next) {
 
 let path = __dirname + "/public"
 app.use("/public", express.static(__dirname + "/public"));
+
+app.use("/name", bodyParser.urlencoded({extended: true}))
 
 app.get("/now",function(req, res, next ) {
 req.time = new Date().toString();
@@ -23,7 +25,12 @@ app.route("/name").get((req, res) => {
   let firstname = req.query.first;
   let lastname = req.query.last;
   res.json({name : firstname +' '+ lastname });
+}).post ((req, res) => {
+  let firstname = req.body.first;
+  let lastname = req.body.last;
+  res.json({name : firstname +' '+ lastname });
 });
+
 
 app.get("/", function(req, res) {
   res.send("Hello World")
